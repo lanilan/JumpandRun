cf562ecf556640fdbf19ce04ac0ee4921c23eeb2
szielinski <szielinski@uni-osnabrueck.de>
2016-12-14 23:20:26 +0100

    Frame wurde bearbeitet

--- src/frame/frame.java
+++ src/frame/frame.java
@@ -1,7 +1,7 @@
 package frame;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by szielinski on 14.12.16.
@@ -10,8 +10,31 @@ public class frame {
 
    public static void main(String[] args){
       JFrame testFrame = new JFrame("TestFrame");
-      testFrame.setSize(800, 600);
-      testFrame.add(new JLabel("Testtesttest"));
+      JButton startgame;   //starts the game
+      JPanel firstPanel;
+      JPanel mainPanel;
+
+      testFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+      int width = 800;
+      int height = 600;
+
+      mainPanel = new JPanel();
+      mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+      firstPanel = new JPanel();
+      mainPanel.setLayout(new GridLayout(4,4));
+
+      startgame = new JButton("start game");
+      startgame.setPreferredSize(new Dimension(100, 60));
+      firstPanel.add(startgame);
+
+
+      mainPanel.add(firstPanel);
+      testFrame.add(mainPanel);
+
+      testFrame.setSize(width, height);
 
       testFrame.setVisible(true);
    }
